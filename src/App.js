dac8ef62e32731d17abcb663c846c4e7b5380710
Thato <sitholethato10@gmail.com>
2022-08-28 20:32:47 +0200

    feat(list-of-comments): Add Title, textfield and button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
+import {
+  Button,
+  FormControl,
+} from 'react-bootstrap';
+
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" 
+        style={{
+          padding: '30px',
+          alignItems: 'flex-start'
+        }}>
+
+        <section
+          id='comments-list'
+          style={{
+            height: 'auto',
+          }}>
+
+        </section>
+
+        <h1
+          style={{
+            color: 'violet'
+          }}>
+
+            Create a comment
+        </h1>
+        <FormControl
+          as ='textarea'/>
+
+        <Button
+          style={{
+            margin: '20px 0 0'
+          }}>
+
+          Submit
+        </Button>
       </header>
     </div>
   );
